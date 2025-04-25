53d93c52d66309a4c2ef706d5827a8b6841817d7
Jason Reed <jcreed@gmail.com>
2025-04-25 11:24:19 -0400

    Add company-completion support

--- emacs-support/local-post.el
+++ emacs-support/local-post.el
@@ -1,3 +1,14 @@
 (setq load-path (cons "/vendor/lean4-mode" load-path))
 
 (require 'lean4-mode)
+(require 'company)
+
+(defun jcreed-lean4-mode-hook ()
+  ;;;; these don't seem to work
+  ;; (setq lsp-inlay-hint-enable t)
+  ;; (lsp-inlay-hints-mode)
+  (setq warning-minimum-level :error)
+  (company-mode)
+  (setq lsp-enable-file-watchers nil)
+  (define-key lean4-mode-map "\M-;" 'company-complete))
+(add-hook 'lean4-mode-hook #'jcreed-lean4-mode-hook)
