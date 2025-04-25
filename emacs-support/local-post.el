cc899420ba125a785cc6eaa6153d6d82f5b9cef3
Jason Reed <jcreed@gmail.com>
2025-04-25 12:58:43 -0400

    Some more conveniences

--- emacs-support/local-post.el
+++ emacs-support/local-post.el
@@ -10,5 +10,8 @@
   (setq warning-minimum-level :error)
   (company-mode)
   (setq lsp-enable-file-watchers nil)
-  (define-key lean4-mode-map "\M-;" 'company-complete))
+  (define-key lean4-mode-map "\M-;" 'company-complete)
+  (define-key lean4-mode-map "\M-." 'lsp-find-definition))
 (add-hook 'lean4-mode-hook #'jcreed-lean4-mode-hook)
+(custom-set-faces
+ '(font-lock-warning-face ((t (:foreground "yellow" :background "#dc322f")))))
